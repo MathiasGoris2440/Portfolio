b2a2085f3587ac8a12bd0a0b3c54ee3f30c29c45
MathiasGoris2440 <mathias.goris@outlook.com>
2025-09-02 12:34:45 +0200

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Cursor from "./Components/Cursor.jsx";
+import Cursor from "./components/Cursor.jsx";
 import {CursorProvider} from "./components/CursorContext.jsx";
 
 createRoot(document.getElementById('root')).render(
